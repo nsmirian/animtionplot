37614617b643dd852e08b725465a1ce4176d2f90
Najmeh Mirian <42173353+nsmirian@users.noreply.github.com>
2023-11-27 18:14:56 +0100

    Create animtionPlot0V.py
    just first step, to make a animation

---
+++ animtionPlot0V.py
@@ -0,0 +1,35 @@
+#https://pythonprogramming.net/live-graphs-matplotlib-tutorial/
+
+
+# animation pyplot
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+style.use('fivethirtyeight')
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+
+#now we write animation function:
+
+
+def animate(i):
+    graph_data = open('example.txt','r').read()
+    lines = graph_data.split('\n')
+    xs = []
+    ys = []
+    for line in lines:
+        if len(line) > 1:
+            x, y = line.split(',')
+            xs.append(float(x))
+            ys.append(float(y))
+    ax1.clear()
+    ax1.plot(xs, ys)
+
+
+
+
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.show()
